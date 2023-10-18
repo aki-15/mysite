872b6cc781af11792475b4446d0db81f5d058c06
aki-15 <114943340+aki-15@users.noreply.github.com>
2023-10-18 12:38:29 -0500

    Add files via upload

---
+++ myprofile.php
@@ -0,0 +1,129 @@
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="アメリカ駐在妻、子育て記録とITスキル向上のためにまとめているサイトです。">
+<title>My Life</title>
+<link rel="icon" href="img/favicon.png">
+<link rel="stylesheet" href="profile2.css">
+</head>
+
+<body>
+	<header>
+    	<div class="container">
+    		<div>
+	    		<img src="img/icon.jpg" alt="アイコン画像" width=160 height=160>
+    		</div>
+    		<div>
+	    		<h1>AT's MyLife</h1>
+    		</div>		
+    	</div>
+	</header>
+		
+	<main>
+		<nav>
+		<h2>目次</h2>
+		<ol>
+			<li><a href="#Profile">自己紹介</a></li>
+			<li><a href="#Program">プログラム</a></li>
+			<li><a href="#memory">思い出の写真</a></li>
+		</ol>
+		</nav>
+		
+		<section id="Profile">
+    		<h2>自己紹介</h2>
+    		<p>アメリカ駐在中です。
+    		<br>プログラム、子育てについて日々勉強しています。</p>
+    		<h3>アメリカ生活略歴</h3>
+    		<table>
+    			<thead>
+    				<tr>
+    					<th>年</th>
+    					<th>出来事</th>
+    				</tr>
+    			</thead>
+    			<tbody>
+    				<tr>
+    					<td>2019.12</td>
+    					<td>主人：先に<a href="https://256times.com" target="_blank">メリーランド州</a>へ渡米</td>
+    				</tr>
+    				<tr>
+    					<td>　***　　　</td>
+    					<td>茨城の田舎でワンオペワーママライフ</td>
+    				</tr>
+    				<tr>
+    					<td>2021.04</td>
+    					<td>私と子供2人：やっと渡米</td>
+    				</tr>
+    			</tbody>
+    		</table>    		
+		</section>
+		
+		<section id="Program">
+    		<h2>
+        		<a href="myportfolio.php" target="_blank">
+        		プログラム
+        		</a>
+    		</h2>
+    		<p>自作したプログラムをまとめています。</p>		
+    		<ul>
+    			<li>HTML</li>
+    			<li>CSS</li>
+    			<li>JavaScript</li>
+    			<li>PHP</li>
+    			<li>React（チャレンジしたい）</li>
+			</ul>
+		</section>
+		
+		<section id="memory">
+			<h2>思い出の写真</h2>
+			<p>お気に入りの写真たち</p>
+		</section>	
+		
+		<section>
+			<h2>お知らせ</h2>
+			<article>
+    			<h3>
+    			<a  href="MyWalkingApp.php" target="_blank">
+    			<span style="color: red;">新機能</span>
+    			</a>
+    			ができました！</h3>
+    			<div style="margin-left: 40px;">
+        			<p>すごい新機能です！すごい新機能です！すごい新機能です！すごい新機能です！すごい新機能です！すごい新機能です！</p>
+        			<footer>
+        				<small>投稿日：<time datetime="2022-11-19 13:00">11月19日</time>投稿者：Taro</small>
+        			</footer>  			
+    			</div>
+  			</article>
+		</section>
+		
+		<section>
+			<h2>メモ</h2>
+			<article>
+				<p>アメリカでの職探しサイト</p>
+				Hired（YoutubeアツEnglishで紹介されていた）。私のようなペーペーSEはベンチャーやスタートアップが良いらしい
+				<footer>
+					<small>投稿日：<time datetime="2023-10-18 12:23">10月18日</time>投稿者：Aki</small>
+				</footer>
+				<p>React</p>
+				フロントエンジニアになりたいならば、Reactを勉強しておくといいらしい（YoutubeアツEnglishで紹介されていた）。
+				<footer>
+					<small>投稿日：<time datetime="2023-10-18 12:23">10月18日</time>投稿者：Aki</small>
+				</footer>
+			</article>
+		</section>
+	</main>
+	
+	<aside>
+    	<h2>広告</h2>
+    	<p>広告です。広告です。広告です。広告です。広告です。広告です。広告です。広告です。広告です。広告です。広告です。</p>	
+	</aside>
+
+	<footer class="copylight">
+		<small>(c) T_aki.2022</small>
+	</footer>
+</body>
+</html>
+
+
+<?php
+?>
